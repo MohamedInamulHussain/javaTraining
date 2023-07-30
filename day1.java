cc5932967bfd82c154f7cba9ab2290051d8263cf
MohamedInamulHussain <140788765+MohamedInamulHussain@users.noreply.github.com>
2023-07-30 09:05:24 -0700

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,124 +1,43 @@
-//1.Demonstration of simple java program 
+//Simple java program 
+
 public class FirstProgram { 
 public static void main(String[] args) { 
 System.out.println("Hello World"); 
 } 
 } 
-//2.program to demonstrate char data type 
-public class CharDemo { 
+
+//program add two numbers
+public class addition { 
 public static void main(String[] args) { 
-// assigning single character literal 
-char ch = 'a'; 
-System.out.println(ch); 
-// assigning number to char 
-char ch1 = 65; 
-System.out.println(ch1); 
-// assigning unicode to char 
-char var1 = '\u00A7'; 
-System.out.println(var1); 
-// Unicode representation 
-char var2 = '\u20AC'; 
-System.out.println(var2); 
-// ASCI code representation 
-int a = 'A'; 
-System.out.println(a); 
+int a=10;
+int b=20;
+int c=a+b;
+System.out.println(c);
 } 
 } 
-//3.Program to demonstrate Data types 
-public class DataTypesDemo { 
+//program with characters and strings
+public class character { 
 public static void main(String[] args) { 
-int value1 = 9 / 2; //integer division 
-float value2 = 101 / 61; 
-double value3 = 10d / 6d; 
-float value4 = 5/2.5f; //float division 
-System.out.println("value 1=" + value1);  
-System.out.println("value 2=" + value2); 
-System.out.println("value 3=" + value3); 
-System.out.println("value 4=" + value4); 
-int marker = 512; 
-//assigning expression to variable  
-double percentage = marker * 0.46f; 
-System.out.println("percentage :" + percentage); 
+char a='A';
+String s="Hello Java";  
+System.out.println(a);
+System.out.println(s);
+//concating char and string
+System.out.println("The final String is :"+a+" "+s);
 } 
 } 
-//4.Program to demonstrate identifiers  
-public class IdentifersDemo { 
+//type conversion
+public class typeConversion { 
 public static void main(String[] args) { 
-// variable declaration - Invalid Identifier Examples 
-/* int for = 13;// error because keyword cannot be identifier 
-System.out.println("value of the number variable is : "+ for);*/ 
-/* 
-int number 2 = 13; //error because do not add space within identifier 
-System.out.println("value of the number variable is : "+ number 2); 
-*/ 
-// error because Identifier can not starts with @, #  
-//int @number3 = 10; 
-//Valid Identifier Examples 
-int $number = 20; //starts with $ 
-System.out.println("value of the number variable is : "+ $number); 
-String studentName="Aniket"; //use camelcase 
-System.out.println("value is : "+ studentName); 
+  int a=10;
+  int b=3;
+  float c=a/b;
+  System.out.println(c);//this will give output as 3 but we need 3.333..
+  float d=(float)a/(float)b;
+  System.out.println(c);//this will give output as 3.333
+  
 } 
 } 
-//5.Program to demonstrate data types  
-public class PrimitiveDataTypesDemo { 
-public static void main(String[] args) { 
-//byte takes 1 byte 
-byte byteMax = 127; 
-byte byteMin = -128; 
-System.out.println("Min range of byte is" + 
-byteMin+"Max range of byte is "+byteMax); 
-//short - 2 bytes 
-short shortMax = 32767; 
-short shortMin = -32768; 
-System.out.println("Minshort range of byte is" + 
-shortMin+"Maxshort range of byte is "+shortMax); 
-//int - 4bytes 
-int maxInt = 2147483647; 
-int minInt = -2147483648; 
-System.out.println("Minint range of byte is" + 
-minInt+"Maxint range of byte is "+maxInt); 
-//long - 8bytes 
-long maxLong = 9223372036854775807L; 
-long minLong = -9223372036854775808L; 
-System.out.println("Minlong range of byte is" + 
-minLong+"Maxlong range of byte is "+maxLong); 
-float f=3234.141243278345f; 
-double d=3456.14124512345678902345678914f; 
-System.out.println("float value is "+f+" double value is "+d); 
-//boolean  
-boolean flag=false; 
-System.out.println("boolean value is "+flag); 
-} 
-} 
-//6.Program to demonstrate type casting between primitive types 
-public class TypeCastingDemo { 
-public static void main(String[] args) { 
-// widening / implicit type casting 
-byte b = 10; 
-int i = b; 
-System.out.println(i); 
-float f = 22.14f; 
-double d = f; 
-System.out.println(d); 
-char ch = 'A'; 
-int i3 = ch; 
-System.out.println(i3); 
-char var1 = '\u00A7'; 
-int i4 = var1; 
-System.out.println(i4); 
-// narrowing/explicit type casting 
-double f1 = 10.52f; 
-long l = (long) f1; 
-System.out.println(l); 
-long l1 = 923372036854775806l; 
-int i2 = (int) l1; 
-System.out.println(i2); 
-float f2 = 34.56f; 
-int i1 = (int) f2; 
-System.out.println(i1); 
-byte b1 = 90; 
-char ch1 = (char) b1; 
-System.out.println(ch1); 
-} 
-} 
+
+
+
